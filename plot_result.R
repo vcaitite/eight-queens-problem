2e03bfc620e12968a3e4930dc22b50d4b7b75e26
GGuedesAB <gustavoguedesab@gmail.com>
2021-03-24 00:30:16 -0300

    Adjust plot function to allow n-queens and print result vector, since now 8x8 tab only shows correct solution for 8-queen

--- plot_result.R
+++ plot_result.R
@@ -2,11 +2,13 @@
 library(imager)
 
 plot_result <- function(queens_position){
+  n = 15
   image <- load.image(file.choose())
   plot(image)
   #coordenadas
   x = 2*c(18,53,88,122,158,192,227,262)
-  for (i in 1:8) {
+  for (i in 1:n) {
+    print(queens_position[i])
     text(x[i],x[queens_position[i]],"Q",cex=2,col = "yellow")
   }
 }
