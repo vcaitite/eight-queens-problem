38d210a0a19fb5f26b57f57ec7c268da7ae396e8
GGuedesAB <gustavoguedesab@gmail.com>
2021-03-24 00:29:09 -0300

    Adjust fitness funtion to allow n-queens

--- fitness_function.R
+++ fitness_function.R
@@ -3,27 +3,28 @@ eight_queens_fitness <- function(solution)
   #teste
   #solution = c(1,5,8,4,2,6,7,3)
   
+  n = 15
   
-  #inicializa um vetor com 64 posicoes preenchido com zeros
-  position_vector = rep.int(0,64)
+  #inicializa um vetor com n^2 posicoes preenchido com zeros
+  position_vector = rep.int(0,n**2)
   
   #variavel auxiliar
   position = 1
   
   #transforma a solution em vetor com 64 posicoes
-  for (i in 1:8) {
+  for (i in 1:n) {
     position_vector[ position + solution[i] -1] = 1;
-    position = position + 8
+    position = position + n
   }
   
-  #tranforma o vetor em uma matriz 8 x 8 
-  queens = matrix(position_vector, nrow=8, ncol=8, byrow = FALSE)
+  #tranforma o vetor em uma matriz n x n 
+  queens = matrix(position_vector, nrow=n, ncol=n, byrow = FALSE)
   
   #variavel para contar os ataques
   total = 0 
   
   #verifica linhas e colunas
-  for (i in 1:8) {
+  for (i in 1:n) {
     #verifica colunas
     total = total + ifelse(sum(queens[,i])>1,1,0)
     #verifica linhas
@@ -35,15 +36,17 @@ eight_queens_fitness <- function(solution)
   z = split(queens,tmp)
   
   #inverte a matriz para ler as outras diagonais
-  queens2 = queens[,8:1]
+  queens2 = queens[,n:1]
   tmp <- row(queens2) - col(queens2)
   y = split(queens2,tmp)
 
-  for (i in 1:15) {
+  i = 1
+  for (v in z) {
     #verifica diagonais
     total = total + ifelse(sum( z[[i]])>1,1,0)
     #verifica diagonais inversa
     total = total + ifelse(sum( y[[i]])>1,1,0)
+    i = i + 1
   }
   return(-total)
   
